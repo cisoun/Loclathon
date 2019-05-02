5f06cc79acc8598656d2a3236af2f0ed88a7dd22
cisoun <cysoun@gmail.com>
2019-05-02 22:19:27 +0200

    Fix wrong behaviour with modals

--- api/api.php
+++ api/api.php
@@ -1,6 +1,6 @@
 <?php
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
+#ini_set('display_errors', 1);
+#error_reporting(E_ALL);
 
 require_once 'config.php';
 
@@ -16,9 +16,7 @@ function sql_build_insert($db, $table, $array)
     $stmt = $db->prepare($query);
 
     foreach ($array as $key => $value)
-    {
         $stmt->bindValue(':' . $key, $value);
-    }
 
     return $stmt;
 }
@@ -116,12 +114,13 @@ function units($data)
     });
 }
 
-
+# Get request's body (JSON).
 $data = array();
 $input = file_get_contents('php://input');
 if ($input !== false)
     $data = json_decode($input, true);
 
+# Route.
 $url = substr($_SERVER['REQUEST_URI'], 5); # Remove '/api/' from URL.'
 header('Content-type: application/json');
 echo(json_encode(call_user_func($url, $data)));
