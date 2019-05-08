f40b73498af46ee02968a5e98b090545e2cbd503
cisoun <cysoun@gmail.com>
2019-05-08 07:59:48 +0200

    Store PayPal Client ID into config

--- api/config.example.php
+++ api/config.example.php
@@ -1,9 +1,15 @@
 <?php
-
 $CONFIG = array(
-    'agents' => [], # Persons email to notify when an order is made.
-    'email' => '', # Email address.
+    # Persons email to notify when an order is made.
+    'agents' => [],
+
+    # Client ID from the PayPal's account (seller).
+    'paypal_id' => '',
+
+    # Email address that will send the confirmation.
+    'email' => '',
+
+    # Logo URL that will be included in the confirmation.
     'logo' => ''
 );
-
 ?>
