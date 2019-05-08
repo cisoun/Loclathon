fcf0b8b1d37657f366f9b8f75fda1ef86edeb01d
cisoun <cysoun@gmail.com>
2019-05-08 07:59:48 +0200

    Store PayPal Client ID into config

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php require_once('api/config.php'); ?>
 <!doctype html>
 <html lang="fr">
 <head>
@@ -188,7 +189,7 @@
   <script src="https://code.jquery.com/jquery-3.4.0.min.js" integrity="sha384-JUMjoW8OzDJw4oFpWIB2Bu/c6768ObEthBMVSiIx4ruBIEdyNSUQAjJNFqT5pnJ6" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  <script src="https://www.paypal.com/sdk/js?client-id=sb&currency=CHF"></script>
+  <script src="https://www.paypal.com/sdk/js?client-id=<?php echo($CONFIG['paypal_id']) ?>&currency=CHF"></script>
   <script src="app.js"></script>
 </body>
 </html>
