3cd1d1d6883de0512fe6dcf95fe90f57b4bb3e2a
cisoun <cysoun@gmail.com>
2021-01-11 22:00:36 +0100

    Add french translations

---
+++ app/lang/fr.php
@@ -0,0 +1,17 @@
+<?php
+return [
+	'contact.title' => 'Formulaire de contact',
+	'contact.error' => [
+		"L'adresse e-mail n'est pas valide!",
+		'Le message est vide.',
+		'La vérification est éronnée. Merci de réessayer.'
+	],
+	'shop.error' => [
+		'<b>Oups !</b> Les adresses mail ne sont pas valides.',
+		'<b>Oups !</b> Les adresses mail ne correspondent pas.',
+		"<b>Désolé !</b> Nous n'avons plus que {{units}} bouteilles en stock !",
+		"<b>Désolé !</b> Vous devez avoir au moins 18 ans pour acheter cet article !<br/>
+		Ou avez-vous oublié de cocher la case de vérification d'âge en bas de la page ?"
+	]
+];
+?>
