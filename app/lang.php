c9e9e7777bb20b4bc766d8c0f923e317ffb9f76c
cisoun <cysoun@gmail.com>
2021-01-11 08:16:43 +0100

    Add Lang class

---
+++ app/lang.php
@@ -0,0 +1,24 @@
+<?php
+class Lang {
+	private static string $PATH = 'lang';
+	private static array  $DATA = [];
+
+	public static function langs() {
+		return glob(self::$PATH . '/*.php');
+	}
+
+	public static function load($lang) {
+		self::$DATA = require_once(self::$PATH . '/' . $lang . '.php');
+	}
+
+	public static function get($key, $params = null) {
+		$text = self::$DATA[$key];
+		if ($params) {
+			foreach ($params as $param => $value) {
+				$text = preg_replace('\{\{\s*' . $param . '\s*\}\}', $value, $text);
+			}
+		}
+		return $text;
+	}
+}
+?>
