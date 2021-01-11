9bd593e1433e49766c394aab8039fa017943d2a0
cisoun <cysoun@gmail.com>
2021-01-11 08:18:34 +0100

    Add Mail class

--- app/mail.php
+++ app/mail.php
@@ -4,39 +4,67 @@ use PHPMailer\PHPMailer\Exception;
 use PHPMailer\PHPMailer\SMTP;
 
 require_once('vendor/autoload.php');
-require_once('config.php');
 
-function mail_send($subject, $to, $body, $html = false)
-{
-    // Instantiation and passing `true` enables exceptions
-    $mail = new PHPMailer(true);
+class Mail {
+	/**
+	 * Send a mail.
+	 *
+	 * Example:
+	 *
+	 * 	Mail::send([
+	 *		'host'       => '127.0.0.1',
+	 *		'user'       => 'mail',
+	 *		'password'   => '...',
+	 *      'subject'    => 'Hello world!',
+	 *      'body'       => '...',
+	 *		'from'       => 'me@example.com',
+	 *		'from_title' => 'Jane Doe',
+	 *		'to'         => ['john@example.com', ...],
+	 *		'bcc'        => ['nate@example.com', ...],
+	 *		// Treat the mail as html.
+	 *	    'html'       => true,
+	 *	]);
+	 *
+	 * @param array $params Parameters of the mail (see above).
+	 */
+	public static function send($params)
+	{
+	    // Instantiation and passing `true` enables exceptions.
+	    $mail = new PHPMailer(true);
 
-    //Server settings
-    $mail->isSMTP();
-    $mail->Host       = $CONFIG['mail_host'];
-    $mail->Username   = $CONFIG['mail'];
-    $mail->Password   = $CONFIG['mail_password'];
-    $mail->SMTPAuth   = true;
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-    $mail->Port       = 587;
+	    // Server settings
+	    $mail->isSMTP();
+	    $mail->Host       = $params['host'];
+	    $mail->Username   = $params['user'];
+	    $mail->Password   = $params['password'];
+	    $mail->SMTPAuth   = true;
+	    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+	    $mail->Port       = 587;
 
-    // Make it quicker.
-    $mail->SMTPOptions = array('ssl' => array('verify_peer_name' => false));
+	    // Make it quicker.
+	    $mail->SMTPOptions = array(
+			'ssl' => array('verify_peer_name' => false)
+		);
 
-    // Recipients
-    $mail->setFrom($CONFIG['mail'], $CONFIG['title']);
-    $mail->addAddress($to);
+	    // Add sender.
+	    $mail->setFrom($params['from'], $params['from_title']);
 
-    // Add the agents.
-    foreach ($CONFIG['agents'] as $agent) {
-        $mail->addBCC($agent);
-    }
+		// Add recipients.
+		foreach ($params['to'] as $recipient) {
+	    	$mail->addAddress($recipient);
+		}
 
-    // Content
-    $mail->isHTML($html);
-    $mail->Subject = $subject;
-    $mail->Body    = $body;
+	    // Add agents.
+	    foreach ($params['bcc'] as $agent) {
+	        $mail->addBCC($agent);
+	    }
 
-    $mail->send();
+	    // Content
+	    $mail->isHTML($params['html']);
+	    $mail->Subject = $params['subject'];
+	    $mail->Body    = $params['body'];
+
+	    $mail->send();
+	}
 }
 ?>
