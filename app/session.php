2fc80891de473d0d70ba7da1c7789c40350e23d8
cisoun <cysoun@gmail.com>
2021-01-25 08:44:46 +0100

    Session: add all() and has()

--- app/session.php
+++ app/session.php
@@ -1,7 +1,15 @@
 <?php
 class Session {
+	public static function all() {
+		return $_SESSION;
+	}
+
 	public static function get($key) {
 		return $_SESSION[$key];
+	}
+
+	public static function has($key) {
+		return array_key_exists($key, $_SESSION);
 	}
 
 	public static function set($key, $value) {
