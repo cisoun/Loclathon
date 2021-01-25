fcc48d19d3b22cdc8bb3e7ba2785c18295a18dc4
cisoun <cysoun@gmail.com>
2021-01-25 08:45:02 +0100

    Validation: add text()

--- app/validation.php
+++ app/validation.php
@@ -3,5 +3,9 @@ class Validation {
 	public static function email($email) {
 		return filter_var($email, FILTER_VALIDATE_EMAIL);
 	}
+
+	public static function text($var) {
+		return trim($var) != false;
+	}
 }
 ?>
