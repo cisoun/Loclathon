eaa81cb63932ca30bc17d7c301c64f2cd683b57d
cisoun <cysoun@gmail.com>
2021-03-17 09:56:38 +0100

    Improve PayPal transaction display in confirmation email

--- app/views/emails/confirmation.php
+++ app/views/emails/confirmation.php
@@ -69,7 +69,7 @@ h3 { margin-top: 2em; }
     <td>
 		<?= $payments[$payment] ?>
 		<?php if ($payment == 'paypal'): ?>
-		<br><?= sprintf($m['paypal_order_id'], $params['paypal_order_id']) ?>
+		<br><small><?= sprintf($m['paypal_order_id'], $params['paypal_order_id']) ?></small>
 		<?php endif; ?>
 	</td>
     <td class="sum">{{ payment_fees }} CHF</td>
