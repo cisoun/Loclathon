1cb687eeeb519e68515958b8f22217baa59f5e03
cisoun <cysoun@gmail.com>
2021-03-20 20:07:58 +0100

    Code formatting

--- index.php
+++ index.php
@@ -8,20 +8,20 @@ if (extension('png|jpg|webp|ttf|css|js|svg|pdf|ico')) {
 
 switch (method()) {
 	case 'GET':
-		route('/',						redirect('/' . Lang::user()));
-		route('/locloise',              redirect('/' . Lang::user() . '/locloise'));
-		route('/:lang',					with_lang(view_cached('loclathon')));
-		route('/:lang/locloise',		with_lang(view_cached('locloise')));
-		route('/:lang/photos',			with_lang(view_cached('albums')));
-		route('/:lang/photos/:year',	with_lang(view_cached('photos')));
-		route('/:lang/shop',			with_lang(call('Shop::show')));
-		route('/:lang/shop/pay',        with_lang(call('Shop::pay')));
-		route('/:lang/shop/confirm',    with_lang(call('Shop::confirm')));
-		route('/:lang/contact',			with_lang(call('Contact::show')));
+		route('/',                   redirect('/' . Lang::user()));
+		route('/locloise',           redirect('/' . Lang::user() . '/locloise'));
+		route('/:lang',              with_lang(view_cached('loclathon')));
+		route('/:lang/locloise',		 with_lang(view_cached('locloise')));
+		route('/:lang/photos',			 with_lang(view_cached('albums')));
+		route('/:lang/photos/:year', with_lang(view_cached('photos')));
+		route('/:lang/shop',			   with_lang(call('Shop::show')));
+		route('/:lang/shop/pay',     with_lang(call('Shop::pay')));
+		route('/:lang/shop/confirm', with_lang(call('Shop::confirm')));
+		route('/:lang/contact',      with_lang(call('Contact::show')));
 		break;
 	case 'POST':
-		route('/:lang/contact',			with_lang(call('Contact::post')));
-		route('/:lang/shop',			with_trim(with_lang(call('Shop::checkout'))));
+		route('/:lang/contact',      with_lang(call('Contact::post')));
+		route('/:lang/shop',         with_trim(with_lang(call('Shop::checkout'))));
 		break;
 }
 
