395996c74863fe9ecaae1cc5c71066feb6be2722
cisoun <cysoun@gmail.com>
2025-08-18 22:38:40 +0200

    Redirect / to /loclathon by default

--- index.php
+++ index.php
@@ -8,11 +8,11 @@ if (extension('png|jpg|webp|ttf|css|js|svg|pdf|ico')) {
 
 switch (method()) {
 	case 'GET':
-		route('/',                        redirect('/' . Lang::user()));
+		route('/',                        redirect('/' . Lang::user() . '/loclathon'));
 		route('/locloise',                redirect('/' . Lang::user() . '/locloise'));
 		route('/shop',                    redirect('/' . Lang::user() . '/shop'));
 		route('/tracker',                 redirect('/' . Lang::user() . '/tracker'));
-		route('/:lang',                   with_lang(view_cached('loclathon')));
+		route('/:lang/loclathon',         with_lang(view_cached('loclathon')));
 		route('/:lang/locloise',          with_lang(view_cached('locloise')));
 		route('/:lang/photos',            with_lang(view_cached('albums')));
 		route('/:lang/photos/:year',      with_lang(view_cached('photos')));
