5f00b41b0b01e00e7bd2e73cd3bb83dd89c882c0
cisoun <cysoun@gmail.com>
2025-09-17 08:49:08 +0200

    Update config.example.php (add german)

--- config.example.php
+++ config.example.php
@@ -1,44 +1,44 @@
 <?php
 
 $CONFIG = [
-    # Mail address of the agents.
-    #   Agents will receive contact forms.
-    'agents'         => ['john@doe.com'],
+	# Mail address of the agents.
+	#   Agents will receive contact forms.
+	'agents'         => ['john@doe.com'],
 
-    # Environment.
-    #   Set variable to false for a production environment.
-    #   Debug environment will disable cached views.
-    'debug'          => true,
+	# Environment.
+	#   Set variable to false for a production environment.
+	#   Debug environment will disable cached views.
+	'debug'          => true,
 
-    # Default locale.
-    'locale'         => 'fr',
+	# Default locale.
+	'locale'         => 'fr',
 
-    # Available locales.
-    'locales'        => ['en', 'fr'],
+	# Available locales.
+	'locales'        => ['de', 'en', 'fr'],
 
-    # Logo URL that will be included in the confirmation.
-    'logo'           => '',
+	# Logo URL that will be included in the confirmation.
+	'logo'           => '',
 
-    # Mail host for outgoing mails.
-    'mail_host'      => gethostbyname('my.host.com'),
+	# Mail host for outgoing mails.
+	'mail_host'      => gethostbyname('my.host.com'),
 
 	# A noreply mail address for outgoing mails.
 	'mail_noreply'   => 'noreply@doe.com',
 
-    # Password for the mail address.
-    'mail_password'  => '',
+	# Password for the mail address.
+	'mail_password'  => '',
 
-    # Mail address for outgoing mails.
-    'mail_user'      => 'jane@doe.com',
+	# Mail address for outgoing mails.
+	'mail_user'      => 'jane@doe.com',
 
-    # Client ID from the PayPal's account (seller).
-    'paypal_id'      => '...',
+	# Client ID from the PayPal's account (seller).
+	'paypal_id'      => '...',
 
-    # Secret key for the PayPal client ID.
-    'paypal_secret'  => '...',
+	# Secret key for the PayPal client ID.
+	'paypal_secret'  => '...',
 
-    # Title of the app.
-    'title'          => 'Le Loclathon',
+	# Title of the app.
+	'title'          => 'Le Loclathon',
 ];
 
 ?>
