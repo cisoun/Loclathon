072f86b31d9eea0223def96f81ade940f90ccb54
cisoun <cysoun@gmail.com>
2026-08-16 18:31:34 +0200

    Shop: add "Tournée" bundle

--- app/controllers/_store.php
+++ app/controllers/_store.php
@@ -197,6 +197,86 @@ return [
 		'pictures'     => [],
 		'restrictions' => [],
 		'parent_id'    => 11
+	], [
+		'id'           => 18,
+		'url'          => 'bundle-tournee',
+		'title'        => 'Bundle "Tournée"',
+		'price'        => 27,
+		'state'        => 0,
+		'description'  => "
+			<p>
+				Le t-shirt à l'effigie de la tournée du Loclathon avec le porte-gobelet et le gobelet.<br>
+			</p>
+			<b>Informations</b>
+			<ul>
+				<li>Blanc</li>
+				<li>Unisex</li>
+				<li>Lavage à 30 °</li>
+				<li>Impression en sérigraphie effectuée avec partenaire local</li>
+			</ul>",
+		'pictures'     => ['18.png'],
+		'restrictions' => [],
+		'parent_id'    => NULL,
+	], [
+		'id'           => 19,
+		'url'          => 'bundle-tournee-xs',
+		'title'        => 'XS',
+		'price'        => 27,
+		'state'        => 0,
+		'description'  => NULL,
+		'pictures'     => [],
+		'restrictions' => [],
+		'parent_id'    => 18
+	], [
+		'id'           => 13,
+		'url'          => 'bundle-tournee-s',
+		'title'        => 'S',
+		'price'        => 27,
+		'state'        => 0,
+		'description'  => NULL,
+		'pictures'     => [],
+		'restrictions' => [],
+		'parent_id'    => 18
+	], [
+		'id'           => 14,
+		'url'          => 'bundle-tournee-m',
+		'title'        => 'M',
+		'price'        => 27,
+		'state'        => 0,
+		'description'  => NULL,
+		'pictures'     => [],
+		'restrictions' => [],
+		'parent_id'    => 18
+	], [
+		'id'           => 15,
+		'url'          => 'bundle-tournee-l',
+		'title'        => 'L',
+		'price'        => 27,
+		'state'        => 0,
+		'description'  => NULL,
+		'pictures'     => [],
+		'restrictions' => [],
+		'parent_id'    => 18
+	], [
+		'id'           => 16,
+		'url'          => 'bundle-tournee-xl',
+		'title'        => 'XL',
+		'price'        => 27,
+		'state'        => 0,
+		'description'  => NULL,
+		'pictures'     => [],
+		'restrictions' => [],
+		'parent_id'    => 18
+	], [
+		'id'           => 17,
+		'url'          => 'bundle-tournee-xxl',
+		'title'        => 'XXL',
+		'price'        => 27,
+		'state'        => 0,
+		'description'  => NULL,
+		'pictures'     => [],
+		'restrictions' => [],
+		'parent_id'    => 18
 	]
 ];
 
